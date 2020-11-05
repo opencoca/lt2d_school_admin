aa52bf266c5019d8a13046cc4012f1df0a1162d8
Alexander Somma <admin@openco.ca>
2020-11-05 11:18:02 -0500

    Adding corsheaders to installed apps and middleware

--- oursite/settings.py
+++ oursite/settings.py
@@ -43,6 +43,7 @@ REST_FRAMEWORK = {
 INSTALLED_APPS = [
     'polls.apps.PollsConfig',
     'classrooms.apps.ClassroomsConfig',
+    'corsheaders',
     'adminsortable2',
     'rest_framework',
     'django.contrib.admin',
@@ -54,6 +55,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
