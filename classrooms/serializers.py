61b6061d3db5b9caae4ecca920827adea034667c
Alexander Somma <admin@openco.ca>
2020-11-12 13:56:37 -0500

    Adding CustomSerializer to allow for simple serialization of methods

--- classrooms/serializers.py
+++ classrooms/serializers.py
@@ -1,6 +1,16 @@
 from .models import Room, Classroom, Teacher, App
 from rest_framework import serializers
 
+class CustomSerializer(serializers.ModelSerializer):
+
+    def get_field_names(self, declared_fields, info):
+        expanded_fields = super(CustomSerializer, self).get_field_names(declared_fields, info)
+
+        if getattr(self.Meta, 'extra_fields', None):
+            return expanded_fields + self.Meta.extra_fields
+        else:
+            return expanded_fields
+
 class TeacherSerializer(serializers.ModelSerializer):
     class Meta:
         model = Teacher
@@ -11,13 +21,14 @@ class AppSerializer(serializers.ModelSerializer):
         model = App
         fields = ['name', 'logo', 'iframe']
 
-class ClassroomSerializer(serializers.ModelSerializer):
+class ClassroomSerializer(CustomSerializer):
     teacher_set = TeacherSerializer(many=True)
     app_set = AppSerializer(many=True)
 
     class Meta:
         model = Classroom
         fields = '__all__'
+        extra_fields = ['name','meet', 'taught_by']
 
 class RoomSerializer(serializers.HyperlinkedModelSerializer):
     classroom = ClassroomSerializer(read_only = True)
