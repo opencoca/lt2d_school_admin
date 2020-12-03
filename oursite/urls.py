267335d41fc70e22bf64f218cd450c2f2a3656b1
Alexander Somma <admin@openco.ca>
2020-12-03 10:27:06 -0500

    Working studio admin with example media file

--- oursite/urls.py
+++ oursite/urls.py
@@ -16,3 +16,7 @@ urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
+
+from django.conf import settings
+from django.conf.urls.static import static
+urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
